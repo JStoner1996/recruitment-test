21476ecb187636b3db1cfd65b6820029b452e621
Jake Stoner <jakestoner123@live.com>
2025-01-17 15:01:45 +0000

    --feat: rendered the employee data in table

--- interviewtest.client/src/App.tsx
+++ interviewtest.client/src/App.tsx
@@ -1,22 +1,54 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import "./Styling/Employees.css";
 
 function App() {
     const [employeeCount, setEmployeeCount] = useState<number>(0);
+  const [employees, setEmployees] = useState<Employee[]>();
 
     useEffect(() => {
         checkConnectivity();
     }, []);
 
-    return (<>
-        <div>Connectivity check: {employeeCount > 0 ? `OK (${employeeCount})` : `NOT READY`}</div>
-        <div>Complete your app here</div>
-    </>);
-
     async function checkConnectivity() {
-        const response = await fetch('api/employees');
+    const response = await fetch("api/employees");
         const data = await response.json();
+    setEmployees(data);
         setEmployeeCount(data.length);
     }
+
+  if (!employees || employees.length === 0) {
+    return <div>No Employees Found.</div>;
+  }
+  return (
+    <>
+      <div>
+        Connectivity check:{" "}
+        {employeeCount > 0 ? `OK (${employeeCount})` : `NOT READY`}
+      </div>
+
+      <div>Complete your app here</div>
+
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee, index) => (
+              <tr key={`${employee.name}-${employee.value}-${index}`}>
+                <td>{employee.name}</td>
+                <td>{employee.value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <button>Increment</button>
+    </>
+  );
 }
 
 export default App;
